09bea28e39d03ce22353ba1de434786a3dea9cc9
elagil <elagil@takanome.de>
2025-01-26 18:37:30 +0100

    fix: improve debug prints

--- src/sink/mod.rs
+++ src/sink/mod.rs
@@ -4,6 +4,7 @@ pub mod policy_engine;
 
 /// Defines the fixed supply that the device policy manager requests from the policy engine.
 #[derive(Debug, Clone, Copy)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub struct FixedSupplyRequest {
     /// The index of the fixed supply (starting at zero) in the list of provided PDOs.
     pub index: u8,
@@ -13,6 +14,7 @@ pub struct FixedSupplyRequest {
 
 /// Types of power source requests that a device can send to the policy engine.
 #[derive(Debug, Clone, Copy)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
 #[non_exhaustive]
 pub enum PowerSourceRequest {
     /// Request a fixed supply voltage.
