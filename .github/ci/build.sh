1dd0986e7c0fdc3db0f73563a0a27aee2dd2aac7
okhsunrog <me@okhsunrog.dev>
2025-05-10 23:12:23 +0300

    fix fmt ci job

--- .github/ci/build.sh
+++ .github/ci/build.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 for dir in usbpd usbpd-traits examples/embassy-nucleo-h563zi examples/embassy-stm32-g431cb;
 do
     pushd $dir
-    cargo fmt
+    cargo fmt --check
     cargo clippy -- -D warnings
     cargo build --release
     popd
