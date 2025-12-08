95ff6cd7d52dc405e99f26af8ceac4754faae69b
okhsunrog <me@okhsunrog.dev>
2025-12-09 02:50:43 +0300

    feat: add EPR example for STM32G431
    Demonstrates USB PD Extended Power Range (EPR) negotiation:
    - Initial SPR power negotiation with EPR capable flag
    - Automatic EPR mode entry when source is EPR capable
    - Requesting 28V @ 4A (112W) EPR power
    - Pretty-printing source capabilities with PDO details
    
    Based on the embassy-stm32-g431cb example.

---
+++ examples/embassy-stm32-g431cb-epr/src/main.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![no_main]
+
+use defmt::info;
+use embassy_executor::Spawner;
+use usbpd_epr_example::power::{self, UcpdResources};
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let mut stm32_config = embassy_stm32::Config::default();
+    // HSI must be enabled for UCPD
+    stm32_config.rcc.hsi = true;
+
+    let p = embassy_stm32::init(stm32_config);
+
+    info!("USB PD EPR Example");
+
+    let ucpd_resources = UcpdResources {
+        pin_cc1: p.PB6,
+        pin_cc2: p.PB4,
+        ucpd: p.UCPD1,
+        rx_dma: p.DMA1_CH1,
+        tx_dma: p.DMA1_CH2,
+    };
+    spawner.spawn(power::ucpd_task(ucpd_resources).unwrap());
+}
